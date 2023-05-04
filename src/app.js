708478590a15bc689a0b0d0113bf3f4cb079d3fe
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-05-04 19:06:49 +1200

    Added unit tests to an Express Application

--- src/app.js
+++ src/app.js
@@ -2,10 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const properties = require("./properties.json");
+const filterProperties = require('./filterProperties')
 
 app.use(cors());
 app.use(express.json());
 
 // FIXME: Create an endpoint that returns all the properties that are not private
-
+app.get("/properties", (request, response) => {
+    const filteredProperties = filterProperties(properties)
+    response.send(filteredProperties);
+})
 module.exports = app;
